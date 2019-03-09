48cac6fb78f1c09313d64b1c47566f6fc932ef0f
Unix&Me <unixnme@gmail.com>
2019-03-09 15:16:31 -0800

    Initial commit; trie

---
+++ trie/trie.py
@@ -0,0 +1,41 @@
+class Trie(object):
+    def __init__(self):
+        self.data = {}
+
+    def __getitem__(self, keys):
+        result = self.data[keys[0]]
+        if keys[1:]:
+            result = result[keys[1:]]
+        return result
+
+
+    def __contains__(self, keys):
+        if keys[0] not in self.data:
+            return False
+        if keys[1:]:
+            return keys[1:] in self.data[keys[0]]
+        return True
+
+
+    def __setitem__(self, keys, value):
+        if len(keys) == 1:
+            self.data[keys[0]] = value
+        else:
+            key = keys[0]
+            if key not in self.data:
+                self.data[key] = Trie()
+                self.data[key][keys[1:]] = value
+            else:
+                self.data[key][keys[1:]] = value
+
+
+import unittest
+class TrieUnitTest(unittest.TestCase):
+    def test_trie(self):
+        trie = Trie()
+        trie[[1,2,3]] = 1
+        self.assertTrue([1,2,3] in trie)
+        self.assertTrue(trie[[1,2,3]] == 1)
+
+        trie[[1,2,3]] += 2
+        self.assertTrue(trie[[1,2,3]] == 3)
